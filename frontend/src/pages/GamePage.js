f200eef4920895f462e59c6d5e4db3498cff8ac2
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-06 16:32:03 +0530

    Automated commit: frontend/src/pages/GamePage.js

---
+++ frontend/src/pages/GamePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GameCanvas from '../components/GameCanvas';
+
+function GamePage() {
+  return (
+    <div className="game-page">
+      <GameCanvas />
+    </div>
+  );
+}
+
+export default GamePage;
